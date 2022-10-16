864e4e408b7431e773d427384aef42c627696f62
bokigolic <bokigolic32@gmail.com>
2022-10-16 16:49:33 -0400

    tracking done

--- postal-react/src/components/TrackingWidget.js
+++ postal-react/src/components/TrackingWidget.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 
+
 const TrackingWidget = () => {
 
   const [trackingId, setTrackingId] = useState("");
@@ -29,6 +30,17 @@ const TrackingWidget = () => {
     statusText = "Status for package " + trackingId + " is: " + status;
   }
 
+  let stepNumber = 0;
+  if (status === "Label Created") {
+    stepNumber = 1;
+  } else if (status === "On the way") {
+    stepNumber = 2;
+  } else if (status === "Out of delivery") {
+    stepNumber = 3;
+  } else if (status === "Delivered") {
+    stepNumber = 4;
+  }
+
 
   return (
     <div>
@@ -43,7 +55,35 @@ const TrackingWidget = () => {
         />
         <button type="button" onClick={handleClick}>Find</button>
       </form>
+      
       <p>{statusText}</p>
+
+      <div className="tracking-status-steps">
+        <div className={stepNumber >= 1 ? "yes" : "no"} >
+          <div className="line"></div>
+          <div className="circle">
+            <div className="text">Label created</div>
+          </div>
+        </div>
+        <div className={stepNumber >= 2 ? "yes" : "no"} >
+          <div className="line"></div>
+          <div className="circle">
+            <div className="text">On the way</div>
+          </div>
+        </div>
+        <div className={stepNumber >= 3 ? "yes" : "no"} >
+          <div className="line"></div>
+          <div className="circle">
+            <div className="text">Out of delivery</div>
+          </div>
+        </div>
+        <div className={stepNumber >= 4 ? "yes" : "no"} >
+          <div className="line"></div>
+          <div className="circle">
+            <div className="text">Delivered</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
