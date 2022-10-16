7b791889e4cdb2696396b0d13ad386be47999b8e
bokigolic <bokigolic32@gmail.com>
2022-10-16 15:29:15 -0400

    backend works

---
+++ postal-backend/backend-readme.md
@@ -0,0 +1,48 @@
+
+
+# HOW TO START json-server backend
+// https://www.npmjs.com/package/json-server#alternative-port
+You must be in folder \postal-backend 
+Then type:
+
+json-server --watch db.json --port 3033
+
+
+Then check in your browser, for example:
+http://localhost:3033/packages
+
+
+## Beleske za kacenje json-server backenda na heroku
+
+https://dashboard.heroku.com/apps/social-json-backend/deploy/heroku-git
+
+https://devcenter.heroku.com/articles/heroku-cli#install-the-heroku-cli
+
+npm install -g heroku
+
+heroku --version
+
+heroku git:remote -a social-json-backend
+
+git add .
+commit -am "make it better"
+git push heroku main
+
+// PRORADILO IZ PRVE!
+Adresa za proveru https://social-json-backend.herokuapp.com
+
+## Kad se izmeni verzija da se nova verzija pojavi na heroku
+
+git status
+git add .
+git commit -m "new version"
+git push
+git status
+
+git add .
+commit -am "make it better"
+git push heroku main
+
+// PRORDAIO I OVAJ UPDATE IZ PRVE!
+// Takodje je proradio na hotgatoru frontend povezan sa heroku backendom
+Adresa za proveru http://socialapp.bojangolic.com/
--- postal-react/src/components/NewParcelWidget.js
+++ postal-react/src/components/NewParcelWidget.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 const NewParcelWidget = () => {
   /*
   Treba nam forma u kojoj se ukucava
@@ -7,6 +10,40 @@ const NewParcelWidget = () => {
     drzava: "srbija"
   },
   */
+  const preset = {
+    street: "",
+    city: "",
+    state: ""
+  };
+
+
+  const [formState, setFormState] = useState(preset);
+
+  const handleChange = (e) => {
+    // console.log(e);
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setFormState({
+      ...formState,
+      [name]: value
+    })
+  };
+
+  const handleClickSubmit = () => {
+    console.log("Novi paket")
+    // CREATE
+    const data = {
+      ...formState
+    };
+    axios.post('http://localhost:3033/packages', data)
+      .then((response)=>{
+        // when axios recive response from backend
+        setFormState({...preset});
+      })
+  };
+
+
   return (
     <div>
       <h1>New Parcel Widget</h1>
@@ -14,11 +51,21 @@ const NewParcelWidget = () => {
       <form>
         <div>
           <label>Street </label>
-          <input type="text" />
+          <input
+            type="text"
+            name="street"
+            value={formState.street}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>City </label>
-          <select>
+          <select
+            name="city"
+            value={formState.city}
+            onChange={handleChange}
+          >
+            <option value="">--Choose city--</option>
             <option value="Rockwille">Rockwille</option>
             <option value="Arlington">Arlington</option>
             <option value="Chickago">Chickago</option>
@@ -27,14 +74,19 @@ const NewParcelWidget = () => {
         </div>
         <div>
           <label>State </label>
-          <select>
+          <select
+            name="state"
+            value={formState.state}
+            onChange={handleChange}
+          >
+            <option value="">--Choose state--</option>
             <option value="Maryland">Maryland</option>
             <option value="Virginia">Virginia</option>
             <option value="Ilinois">Ilinois</option>
             <option value="Arizona">Arizona</option>
           </select>
         </div>
-        <button>Send parcel</button>
+        <button type="button" onClick={handleClickSubmit}>Send parcel</button>
 
         <p>TODO: prikazati novi tracking id a mozda i sva polja nalepnice</p>
       </form>
